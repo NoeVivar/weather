e6b8c58609d8cf9f0c66a6aeb44610a7ac99b2bf
NoeVivar <nvivar@vcd-ceb131-11.eastern.ewu.edu>
2017-03-13 09:42:17 -0700

    weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-// YOUR SCRIPTS GO HERE
+// Get the weather
+$.simpleWeather({
+  location: 97982,
+  unit: 'f',
+  success:function(weather){
+//   Weather
+   console.log(weather); 
+// Display    
+    
+ $('.city').text(weather.city);   
+ $('.state').text(weather.region);
+ $('.temp').text(weather.temp);
+ $('.country').text(weather.country)
+ $('.thumb img').attr('src', weather.image);
+    
+  },
+  error: function(error){
+  console.log('No Weather Found');
+  }
+  
